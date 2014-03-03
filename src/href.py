0d4e01bb31a368e671f6f92fed9b9d69fbe495da
Wenzhou <wenzhouw1989@gmail.com>
2014-03-02 22:36:06 -0600

    modified href.py
    fixed some href bugs

--- src/href.py
+++ src/href.py
@@ -9,9 +9,17 @@ for link in soup.find_all('link'):
         href='http://en.wikipedia.org'
         href+=link['href']
         link['href']=href
+    if link['href'][:2]=='//':
+        href='http:'
+        href+=link['href']
+        link['href']=href
 for link in soup.find_all('a'):
     if link.get('href')!=None and link['href'][0]!='#' and link['href'][:2]!='//' and link['href'][:3]!='www' and link['href'][:4]!='http':
         href='http://en.wikipedia.org'
+        href+=link['href']
+        link['href']=href
+    if link.get('href')!=None and link['href'][:2]=='//':
+        href='http:'
         href+=link['href']
         link['href']=href
 #html=soup.prettify('utf-8')
